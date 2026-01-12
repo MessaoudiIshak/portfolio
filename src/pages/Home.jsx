c6adf1fffc92870706bbec25056908e2e0f8ac15
MessaoudiIshak <ishakabderrahmanem@gmail.com>
2026-01-12 22:04:09 +0100

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import AboutSection from '@/components/AboutSection'
 import SkillsSection from '@/components/SkillsSection'
 import ProjectsSection from '@/components/ProjectsSection'
 import ContactSection from '@/components/ContactSection'
+import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
@@ -20,7 +21,7 @@ export default function Home() {
             <ProjectsSection/>
             <ContactSection/>
         </main>
-
+        <Footer/>
     </div>
   )
 }
