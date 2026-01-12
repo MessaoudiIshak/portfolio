266e12547b3462880364d0d5430c64aa090ed73d
MessaoudiIshak <ishakabderrahmanem@gmail.com>
2026-01-12 03:26:35 +0100

    background and darkmode done

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from 'react';
+import {Sun} from 'lucide-react';
+import {Moon} from 'lucide-react';
+import { cn } from '../lib/utils';
+
+export default function ThemeToggle() {
+const [isDarkMode, setIsDarkMode] = useState(false);
+
+useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');   
+    if (storedTheme === 'dark' ) {
+        setIsDarkMode(true);
+        document.documentElement.classList.add('dark');
+        
+    }   else { document.documentElement.classList.remove('dark');
+        setIsDarkMode(false);
+    }
+}, []);
+
+const ToggleTheme = () => {
+    if (isDarkMode) {
+        document.documentElement.classList.remove('dark');
+        setIsDarkMode(false);
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        setIsDarkMode(true);
+        localStorage.setItem('theme', 'dark');
+    }
+}
+  return (
+    <button onClick={ToggleTheme} className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outlin-hidden"
+      )}>{""}
+    {isDarkMode ?
+        ( <Sun className='h-6 w-6 text-yellow-300' /> )
+        :(
+         <Moon className='h-6 w-6 text-blue-900'/>
+        )}
+        </button>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ThemeToggle from '@/components/ThemeToggle'
+import StarBackground from '@/components/StarBackground'
+export default function Home() {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+        <ThemeToggle />
+        <StarBackground/>
+        <NavBar/>
+
+    </div>
+  )
+}
