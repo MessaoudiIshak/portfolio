5e5896498f9189b1e2856c1ad723960a404135bf
MessaoudiIshak <ishakabderrahmanem@gmail.com>
2026-01-12 16:50:30 +0100

    about section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import ThemeToggle from '@/components/ThemeToggle'
 import StarBackground from '@/components/StarBackground'
 import NavBar from '@/components/NavBar'
 import HeroSection from '@/components/HeroSection'
+import AboutSection from '@/components/AboutSection'
 export default function Home() {
   return (
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
@@ -11,6 +12,7 @@ export default function Home() {
         <NavBar/>
         <main>
             <HeroSection/>
+            <AboutSection/>
         </main>
 
     </div>
